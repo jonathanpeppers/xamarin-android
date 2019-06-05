1d81c8bb0cd78a0e6c7a80214cb8ffadc6ae8e3a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-06-05 18:10:39 -0500

    [Xamarin.Android.Build.Tasks] #deletebinobj going from 15.9 to master
    I setup a new #deletebinobj "regression protection" test:
    
    * Created the Master/Detail Xamarin.Forms template from Visual Studio
      2017 15.9
    * Built it with 15.9
    * Zipped it up, and uploaded to Azure
    
    I created 4 zips: Debug & Release, Windows & Darwin.
    
    Next I created an `ExistingProject` type in `Xamarin.ProjectTools`, so
    I could unzip these files and build them.
    
    Unfortunately this found a bug:
    
        Task CompileToDalvik
        ...
        Xamarin.Android.Common.targets(2805,3): java.lang.IllegalArgumentException: already added :  Lmono/android/Seppuku;
    
    `Seppuku.java` was another file we missed when creating the
    `_CleanupOldStaticResources` target.
    
    This fix we can hopefully backport to 16.2. I plan to make further
    changes to `DeleteBinObjTest.cs` in other PRs. It should start apps
    and connect the debugger like we are doing in other tests.

---
+++ tests/MSBuildDeviceIntegration/Tests/DeleteBinObjTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Xamarin.ProjectTools;
+using Xamarin.Tools.Zip;
+
+namespace Xamarin.Android.Build.Tests
+{
+	[TestFixture]
+	public class DeleteBinObjTest : DeviceTest
+	{
+		const string BaseUrl = "https://xamjenkinsartifact.azureedge.net/mono-jenkins/xamarin-android-test/";
+		readonly DownloadedCache Cache = new DownloadedCache ();
+
+		string HostOS => IsWindows ? "Windows" : "Darwin";
+
+		void RunTest (string name, string csproj, string version, bool isRelease)
+		{
+			var configuration = isRelease ? "Release" : "Debug";
+			var zipPath = Cache.GetAsFile ($"{BaseUrl}{name}-{version}-{configuration}-{HostOS}.zip");
+			using (var builder = CreateApkBuilder (Path.Combine ("temp", TestName)))
+			using (var zip = ZipArchive.Open (zipPath, FileMode.Open)) {
+				var projectDir = Path.Combine (Root, builder.ProjectDirectory);
+				if (Directory.Exists (projectDir))
+					Directory.Delete (projectDir, recursive: true);
+				zip.ExtractAll (projectDir);
+
+				var project = new ExistingProject {
+					ProjectFilePath = Path.Combine (projectDir, csproj),
+				};
+				
+				if (HasDevices) {
+					Assert.IsTrue (builder.Install (project, doNotCleanupOnUpdate: true, saveProject: false), "Install should have succeeded.");
+				} else {
+					Assert.IsTrue (builder.Build (project, doNotCleanupOnUpdate: true, saveProject: false), "Build should have succeeded.");
+				}
+			}
+		}
+
+		[Test]
+		public void HelloForms ([Values (false, true)] bool isRelease)
+		{
+			RunTest (nameof (HelloForms), Path.Combine ("HelloForms.Android", "HelloForms.Android.csproj"), "15.9", isRelease);
+		}
+	}
+}
