cc37f9d38abc0160ec869b88cbddf1c0a047eccd
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-17 16:33:17 -0500

    New <JniMarshalMethodGen/> MSBuild task
    The previous <Exec/> call was looking in linksrc...
    
    At this point it looked easier to port the MSBuild-ese to C# than edit the crazy expression.

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/JniMarshalMethodGen.cs
@@ -0,0 +1,85 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Android.Tools;
+
+namespace Xamarin.Android.Tasks
+{
+	/// <summary>
+	/// Invokes jnimarshalmethod-gen.exe
+	/// </summary>
+	public class JniMarshalMethodGen : ToolTask
+	{
+		protected override string ToolName => "jnimarshalmethod-gen";
+
+		public override string ToolExe {
+			get { return OS.IsWindows ? "mono.exe" : "mono"; }
+			set { base.ToolExe = value; }
+		}
+
+		[Required]
+		public string MonoAndroidBinDirectory { get; set; }
+
+		[Required]
+		public string MonoAndroidToolsDirectory { get; set; }
+
+		[Required]
+		public string MonoAndroidLibDirectory { get; set; }
+
+		[Required]
+		public string [] TargetFrameworkDirectories { get; set; }
+
+		[Required]
+		public string [] Assemblies { get; set; }
+
+		[Required]
+		public string JdkJvmPath { get; set; }
+
+		public string ExtraArguments { get; set; }
+
+		public override bool Execute ()
+		{
+			var mono_path = new List<string> {
+				Path.Combine (MonoAndroidBinDirectory, "bcl"),
+				Path.Combine (MonoAndroidBinDirectory, "bcl", "Facades"),
+			};
+			mono_path.AddRange (TargetFrameworkDirectories);
+
+			foreach (var assembly in Assemblies) {
+				var dir = Path.GetDirectoryName (assembly);
+				if (!mono_path.Contains (dir)) {
+					mono_path.Add (dir);
+				}
+			}
+
+			EnvironmentVariables = new [] {
+				"DYLD_LIBRARY_PATH=" + MonoAndroidLibDirectory,
+				"MONO_CONFIG=" + Path.Combine (MonoAndroidBinDirectory, "mono.config"),
+				"MONO_PATH=" + string.Join (Path.PathSeparator.ToString (), mono_path),
+			};
+
+			return base.Execute ();
+		}
+
+		protected override string GenerateFullPathToTool () => Path.Combine (MonoAndroidBinDirectory, ToolExe);
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			var cmd = new CommandLineBuilder ();
+
+			cmd.AppendSwitch ("--debug");
+			cmd.AppendFileNameIfNotNull (Path.Combine (MonoAndroidToolsDirectory, ToolName + ".exe"));
+			cmd.AppendSwitchIfNotNull ("--jvm ", JdkJvmPath);
+
+			if (!string.IsNullOrEmpty (ExtraArguments))
+				cmd.AppendSwitch (ExtraArguments);
+
+			foreach (var assembly in Assemblies) {
+				cmd.AppendFileNameIfNotNull (assembly);
+			}
+
+			return cmd.ToString ();
+		}
+	}
+}
