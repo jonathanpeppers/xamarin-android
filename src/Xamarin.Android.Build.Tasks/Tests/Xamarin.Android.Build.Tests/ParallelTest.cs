7671d47d7f74b560bd8c3cfd4a57f9ccba71c8d4
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-02-04 17:08:02 -0600

    WIP spotted the first couple issues
    These might not be the best fixes, but I just made some things skip for now.
    
    I am now hitting an error in Xamarin.Forms:
    
        XamlGTask
        App.xaml The process cannot access the file 'C:\src\xamarin-android\bin\TestDebug\temp\ParallelDesignTimeBuild\obj\Debug\monoandroid10.0\App.xaml.g.cs' because it is being used by another process.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/ParallelTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/ParallelTest.cs
@@ -34,20 +34,48 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[Test]
+		[Repeat (5)]
 		public void ParallelDesignTimeBuild ()
 		{
+			var parameters = new [] { "SkipCompilerExecution=True", "ProvideCommandLineArgs=True" };
+
 			Parallel.For (0, 10, i => {
 				using (var builder = CreateApkBuilder (cleanupOnDispose: false)) {
 					builder.AutomaticNuGetRestore = false;
 					builder.BuildLogFile = null;
 					builder.BinLogFile = $"msbuild{i}.binlog";
-					Assert.IsTrue (builder.DesignTimeBuild (project, doNotCleanupOnUpdate: true), $"DesignTimeBuild {i} failed");
+					Assert.IsTrue (builder.DesignTimeBuild (project, "CompileDesignTime", doNotCleanupOnUpdate: true, parameters: parameters),
+						$"DesignTimeBuild {i} failed");
 				}
 			});
 		}
 
-		class Project : XamarinAndroidApplicationProject
+		class Project : XASdkProject
 		{
+			XamarinFormsAndroidApplicationProject project;
+
+			public Project () : base ("0.0.1")
+			{
+				// Create a Xamarin.Forms project and copy the item groups over
+				project = new XamarinFormsAndroidApplicationProject ();
+				foreach (var package in project.PackageReferences) {
+					PackageReferences.Add (package);
+				}
+				foreach (var group in project.ItemGroupList) {
+					foreach (var item in group) {
+						if (GetItem (item.Include ()) == null) {
+							if (item.BuildAction == "Compile" ||
+								item.BuildAction == "EmbeddedResource" ||
+								item.BuildAction == "AndroidResource") {
+								Sources.Add (item);
+							} else {
+								OtherBuildItems.Add (item);
+							}
+						}
+					}
+				}
+			}
+
 			bool shouldPopulate = true;
 
 			public override bool ShouldPopulate => shouldPopulate;
